42c21dbc7dd5d7495893a76b8933b4a2a6734418
Kaushal Apastambh <apastambhkaushal2001@gmail.com>
2026-07-05 23:05:55 -0700

    Set up CORS and allow credentials for cross-origin requests

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
 const app = express();
-const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const { authRouter } = require("./router/auth");
 const { profileRouter } = require("./router/profile");
 const { requestRouter } = require("./router/request");
 const { userRouter } = require("./router/user");
-require("dotenv").config();
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
