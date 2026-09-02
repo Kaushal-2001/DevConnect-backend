3244f0d5ce902e91b906af406ed4809ea57ece2c
Kaushal Apastambh <apastambhkaushal2001@gmail.com>
2026-09-02 09:38:32 -0700

    Add AWS SES client and email sending utility

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,52 @@
+const { SendEmailCommand } = require("@aws-sdk/client-ses");
+const {sesClient} = require("./sesClient")
+
+const createSendEmailCommand = (toAddress, fromAddress) => {
+  return new SendEmailCommand({
+    Destination: {
+      CcAddresses: [
+      ],
+      ToAddresses: [
+        toAddress,
+      ],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: "<h1>This is the email body</h1>",
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: "This is the text format email",
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Hello form SES",
+      },
+    },
+    Source: fromAddress,
+    ReplyToAddresses: [
+    ],
+  });
+};
+
+const run = async () => {
+  const sendEmailCommand = createSendEmailCommand(
+    "apastambhkaushal2001@gmail.com",
+    "apastambhkaushal123@gmail.com",
+  );
+
+  try {
+    return await sesClient.send(sendEmailCommand);
+  } catch (caught) {
+    if (caught instanceof Error && caught.name === "MessageRejected") {
+      const messageRejectedError = caught;
+      return messageRejectedError;
+    }
+    throw caught;
+  }
+};
+
+module.exports = { run };
